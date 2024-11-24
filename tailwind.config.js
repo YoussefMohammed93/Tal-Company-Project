9e8e498ee6b92c7d505e6123939bfa3190fa3631
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-11-24 13:02:05 +0200

    Services section + responsive + some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: [
-    "./*.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "light-streak": "light-streak 0.5s ease-out forwards",
+      },
+      keyframes: {
+        "light-streak": {
+          "0%": { left: "-100%", opacity: 0.2 },
+          "50%": { left: "50%", opacity: 0.6 },
+          "100%": { left: "200%", opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [],
 };
